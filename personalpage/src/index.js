785523601b652f0dd88a3b330e06a19e310558b5
Annabella Malatinszki <a.malatinszki@gmail.com>
2021-05-13 12:19:52 +0200

    wrap App component in theme

--- personalpage/src/index.js
+++ personalpage/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+
+import Theme from './theme/Theme';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={Theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
